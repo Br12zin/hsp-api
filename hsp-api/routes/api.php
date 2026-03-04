b992b95e0a9100eefd10c9a9b68a2d13971c9121
Br12zin <Brunolobo2110@gmail.com>
2026-03-04 17:30:34 -0300

    feat(atualizado): a caminho do deploy, feito já instalação do railway

--- hsp-api/routes/api.php
+++ hsp-api/routes/api.php
@@ -1,6 +1,13 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
 
+// Rotas públicas
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+// Rotas protegidas (exigem token)
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [AuthController::class, 'user']);
+});
