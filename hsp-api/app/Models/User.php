d3de9cd49e5f13af7d884dd6aafa98125a208cc9
Br12zin <Brunolobo2110@gmail.com>
2026-03-09 10:06:06 -0300

    feat(tentando arrumar)

---
+++ hsp-api/app/Models/User.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens; // <-- ADICIONE ESTA LINHA
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable; // <-- ADICIONE HasApiTokens AQUI
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
